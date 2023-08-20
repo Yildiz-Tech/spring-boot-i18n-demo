1b4d52f535bfb5a43b69745943697299bd378455
İzzet Kılıç <izzet.kilic@yildiztech.com.tr>
2023-08-20 21:11:48 +0300

    removed unused import

--- src/main/java/com/yildiztech/i18n/config/MessageSourceConfig.java
+++ src/main/java/com/yildiztech/i18n/config/MessageSourceConfig.java
@@ -1,7 +1,6 @@
 package com.yildiztech.i18n.config;
 
 import java.nio.charset.StandardCharsets;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
