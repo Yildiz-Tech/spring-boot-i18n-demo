1b7edd50b688f85ba5039e612f4be05b9810b76f
İzzet Kılıç <izzet.kilic@yildiztech.com.tr>
2023-08-21 09:21:06 +0300

    http header converted to constant

--- src/main/java/com/yildiztech/i18n/config/CustomLocaleResolver.java
+++ src/main/java/com/yildiztech/i18n/config/CustomLocaleResolver.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @Configuration
@@ -16,10 +17,10 @@ public class CustomLocaleResolver extends AcceptHeaderLocaleResolver {
 
   @Override
   public Locale resolveLocale(HttpServletRequest request) {
-    if (StringUtils.isEmpty(request.getHeader("Accept-Language"))) {
+    if (StringUtils.isEmpty(request.getHeader(HttpHeaders.ACCEPT_LANGUAGE))) {
       return Locale.forLanguageTag(DEFAULT_LOCALE);
     }
-    var list = Locale.LanguageRange.parse(request.getHeader("Accept-Language"));
+    var list = Locale.LanguageRange.parse(request.getHeader(HttpHeaders.ACCEPT_LANGUAGE));
     return Locale.lookup(list, LOCALES);
   }
 }
